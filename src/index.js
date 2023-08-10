dcbb2a84a949ab4355524aa498e26e6ea94a7f7e
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-08-10 07:37:26 +0430

    hearth icon selected

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './style.css';
 import { loadImage } from './modules/Api';
+// index.js
+
+const containerAllImages = document.querySelector('.hero'); // Select the parent element
+containerAllImages.addEventListener('click', (event) => {
+  const heartIcon = event.target.closest('.hearth');
+  if (heartIcon) {
+    console.log('Heart clicked');
+    heartIcon.style.color = 'red';
+  
+    // localStorage.setItem('colorCurrentItem','red');
+  }
+});
 
 loadImage();
