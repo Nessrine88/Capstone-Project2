8bb70784afc5489079cac07dd20f486409213353
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-08-12 00:44:50 +0430

    test added

---
+++ src/sum.test.js
@@ -0,0 +1,36 @@
+
+import cardCounter from './sum'; // Adjust the import path if needed
+
+test('should update the card counter with the correct number of cards', () => {
+  document.body.innerHTML = `
+    <div class="card-container">
+      <div class="containerLike"></div>
+      <div class="containerLike"></div>
+      <div class="containerLike"></div>
+    </div>
+  `;
+
+  expect(cardCounter()).toBe(3);
+});
+test('should return 4 cards', () => {
+  document.body.innerHTML = `
+    <div class="card-container">
+      <div class="containerLike"></div>
+      <div class="containerLike"></div>
+      <div class="containerLike"></div>
+      <div class="containerLike"></div>
+    </div>
+  `;
+  cardCounter();
+  expect(cardCounter()).not.toBe(5);
+});
+
+
+test('should return 0 cards', () => {
+  document.body.innerHTML = `
+    <div class="card-container">
+    </div>
+  `;
+  cardCounter();
+  expect(cardCounter()).toBe(0);
+});
